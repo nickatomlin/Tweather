c9d625c99a5a256f04a6cce16a9d3d4f1150fdf2
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-11-14 14:17:20 -0500

    MAJOR : finds tweets about weather near Harvard University.

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from TwitterSearch import *
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['because reasons']) # let's define all words we would like to have a look for
+    tso.set_keywords(['weather']) # let's define all words we would like to have a look for
     tso.set_language('en')
+    tso.set_geocode(42.3744, -71.1169, 10, True)
     tso.set_include_entities(False) # and don't give us all those entity information
 
     ts = TwitterSearch(
@@ -14,7 +15,7 @@ try:
         
                        # this is where the fun actually starts :)
     for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+        print( '@%s tweeted: %s at %s' % ( tweet['user']['screen_name'], tweet['text'], tweet['geo'] ) )
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
