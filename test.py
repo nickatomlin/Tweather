4a433f524af8d9c625af75d3ceb1441d82234917
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-11-14 16:18:01 -0500

    Added keywords
    also fixed or-concatenation of keywords

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from TwitterSearch import *
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['weather']) # let's define all words we would like to have a look for
+    tso.set_keywords(['weather', 'rain', 'rainy', 'raining', 'sunny', 'cloudy'
+                      'foggy', 'fog', 'hail', 'hailing', 'snow.' 'snowing'], or_operator=True)
     tso.set_language('en')
-    tso.set_geocode(42.3744, -71.1169, 10, True)
+    tso.set_geocode(42.3744, -71.1169, 5, True)
     tso.set_include_entities(False) # and don't give us all those entity information
 
     ts = TwitterSearch(
@@ -12,10 +13,8 @@ try:
                        access_token = '4188038303-tlz5WruG6b78FugN2CBq9lsuRa5IJYtnzxgRMwr',
                        access_token_secret = 'bLLK8LDSyAVUL9EVtQkNGDHx8DP1FGRleNwi5FVqMrNVi'
                        )
-        
-                       # this is where the fun actually starts :)
     for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s at %s' % ( tweet['user']['screen_name'], tweet['text'], tweet['geo'] ) )
+        print( '@%s tweeted: %s at %s at %s' % ( tweet['user']['screen_name'], tweet['text'], tweet['geo'], tweet['created_at'] ) )
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
