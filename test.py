77af7c077370e12db14f7f87bbfb284ff3f27fa7
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-11-14 21:11:56 -0500

    good luck chris

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import datetime
 import operator
 
 def weather_desc(tweets):
@@ -26,126 +27,106 @@ def weather_desc(tweets):
              'foggy': 0.0,
              'cloudy': 0.0}
 
+    texts = []
+
     for tweet in tweets:
         if "windy" in tweet['text'] or "wind" in tweet['text']:
             notions['windy'] += 5.0
 
-    for tweet in tweets:
         if "comfortable" in tweet['text']:
             notions['comfortable'] += 5.0
 
-    for tweet in tweets:
         if "refreshing" in tweet['text']:
             notions['refreshing'] += 5.0
 
-    for tweet in tweets:
         if "beautiful" in tweet['text'] or "nice weather" in tweet['text']:
             notions['beautiful'] += 5.0
 
-    for tweet in tweets:
         if "ominous" in tweet['text']:
             notions['ominous'] += 5.0
             notions['stormy'] += 3.5
             notions['miserable'] += 3.0
 
-    for tweet in tweets:
         if "miserable" in tweet['text']:
             notions['miserable'] += 5.0
 
-    for tweet in tweets:
         if "foggy" in tweet['text'] or "fog" in tweet['text']:
             notions['foggy'] += 5.0
             notions['humid'] += 2.5
             notions['muggy'] += 1.0
 
-    for tweet in tweets:
         if "severe" in tweet['text'] or "thunderstorm" in tweet['text'] or "storm" in tweet['text']:
             notions['severe'] += 5.0
-            notions['storm'] += 4.0
+            notions['stormy'] += 4.0
 
-    for tweet in tweets:
         if "humid" in tweet['text']:
             notions['humid'] += 5.0
             notions['muggy'] += 2.0
 
-    for tweet in tweets:
         if "muggy" in tweet['text']:
             notions['muggy'] += 5.0
             notions['humid'] += 3.5
 
-    for tweet in tweets:
         if "stormy" in tweet['text'] or "thunderstorm" in tweet['text'] or "storm" in tweet['text']:
             notions['stormy'] += 5.0
             notions['severe'] += 3.5
 
-    for tweet in tweets:
         if "tornado" in tweet['text'] or "hurricane" in tweet['text']:
             notions['stormy'] += 10.0
             notions['severe'] += 6.5
 
-    for tweet in tweets:
         if "flooding" in tweet['text'] or "flood" in tweet['text']:
             notions['flooding'] += 5.0
 
-    for tweet in tweets:
         if "warm" in tweet['text']:
             temps['warm'] += 5.0
             temps['hot'] += 1.5
 
-    for tweet in tweets:
         if "hot" in tweet['text']:
             temps['hot'] += 5.0
             temps['warm'] += 3.0
 
-    for tweet in tweets:
         if "cold" in tweet['text']:
             temps['cold'] += 5.0
 
-    for tweet in tweets:
         if "freezing" in tweet['text']:
             temps['freezing'] += 5.0
             temps['cold'] += 3.0
 
-    for tweet in tweets:
         if "clear" in tweet['text']:
             precs['clear'] += 5.0
 
-    for tweet in tweets:
         if "overcast" in tweet['text']:
             precs['overcast'] += 5.0
 
-    for tweet in tweets:
         if "raining" in tweet['text']:
             precs['raining'] += 5.0
 
-    for tweet in tweets:
         if "sunny" in tweet['text']:
             precs['sunny'] += 5.0
 
-    for tweet in tweets:
         if "hailing" in tweet['text']:
             precs['hailing'] += 5.0
             precs['snowing'] += 1.0
 
-    for tweet in tweets:
         if "snowing" in tweet['text']:
             precs['snowing'] += 5.0
 
-    for tweet in tweets:
         if "foggy" in tweet['text']:
             precs['foggy'] += 5.0
 
-    for tweet in tweets:
         if "cloudy" in tweet['text']:
             precs['cloudy'] += 5.0
 
-    for tweet in tweets:
         if "precipitation" in tweet['text']:
             precs['raining'] += 2.0
             precs['snowing'] += 1.5
             precs['hailing'] += 1.0
 
-    return (max(notions, key=notions.get), max(temps, key=temps.get), max(precs, key=precs.get))
+        texts.append(tweet['text'])
+
+    return ((max(notions, key=notions.get), max(temps, key=temps.get), max(precs, key=precs.get)),
+            (texts[0], texts[1], texts[2]))
 
 from TwitterSearch import *
 try:
@@ -153,6 +134,7 @@ try:
     tso.set_keywords(['weather', 'rain', 'rainy', 'raining', 'sunny', 'cloudy', 'cold', 'colder',
                       'foggy', 'fog', 'hail', 'hailing', 'snow', 'snowing',
                       'wind', 'windy', 'overcast'], or_operator=True)
+    tso.set_until(datetime.date(2015, 11, 13))
     tso.set_language('en')
     tso.set_geocode(42.3744, -71.1169, 5, True)
     tso.set_include_entities(False)
@@ -165,6 +147,7 @@ try:
                        )
 
     print(weather_desc(ts.search_tweets_iterable(tso)))
+
     #for tweet in ts.search_tweets_iterable(tso):
         
         #print( '@%s tweeted: %s at %s at %s' % ( tweet['user']['screen_name'], tweet['text'], tweet['geo'], tweet['created_at'] ) )
